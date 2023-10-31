30c4a376b6a44f18ae7b494dc0ce4c3d9528586e
YOUNESS BAIT <126654217+younessbait@users.noreply.github.com>
2023-10-31 23:30:29 +0100

    Add files via upload

---
+++ Tutorials.js
@@ -0,0 +1,8 @@
+let img = document.querySelector(".img");
+function phones(phone) {
+  img.src = phone;
+}
+let contianer = document.querySelector(".contianer");
+function colors(color) {
+    contianer.style.background = color;
+}
